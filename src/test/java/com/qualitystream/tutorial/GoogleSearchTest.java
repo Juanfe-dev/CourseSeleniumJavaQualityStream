c05a7432474b8e53c8a6bda6209e79717877919d
JuanFe <juan.gomez164@udea.edu.co>
2022-06-16 16:08:30 -0500

    Resultado final Cap4.0

--- src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
+++ src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
@@ -3,7 +3,9 @@ package com.qualitystream.tutorial;
 import static org.junit.Assert.*;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,11 +14,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleSearchTest {
 
 	private WebDriver driver;
-	
+	By videoLinkLocator = By.cssSelector("a[href='https://www.youtube.com/watch?v=R_hh3jAqn8M']");
 	@Before
 	public void setUp() {
 		
@@ -34,9 +40,33 @@ public class GoogleSearchTest {
 		WebElement searchbox = driver.findElement(By.name("q")); // By.name es un localizador
 		searchbox.clear();
 		searchbox.sendKeys("quality-stream Introducción a la Automatización de Pruebas de SoftWare");
-		searchbox.submit();
+		searchbox.submit(); // es como dar enter
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// implicit wait
+			// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); 
+		// explicit wait
+			//reemplazo con un explicit wait a modo de practica WebDriverWait:ExpectedConditions
+			// En la version antigua no era necesario el Duration.ofSeconds(10), solo con ingresar el entero funcionaba
+			//WebDriverWait ewait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			//Espera hasta que el titulo tenga: quality-stream hasta los 10 segundos
+			//ewait.until(ExpectedConditions.titleContains("quality-stream"));
+		
+			// FluentWait se define en el documento 
+				Wait<WebDriver> fwait = new FluentWait<WebDriver>(driver)
+						.withTimeout(Duration.ofSeconds(10))
+						.pollingEvery(Duration.ofSeconds(2))
+						.ignoring(NoSuchElementException.class);
+				WebElement video = fwait.until(new Function<WebDriver, WebElement>(){
+					public WebElement apply(WebDriver driver) {
+						return driver.findElement(videoLinkLocator);
+					}
+				}
+				);
+				// verificamos con junit
+				assertTrue(driver.findElement(videoLinkLocator).isDisplayed());
+				
+				
+		
 		
 		// revisamos resultados de la busqueda con junit
 		assertEquals("quality-stream Introducción a la Automatización de Pruebas de SoftWare - Buscar con Google", driver.getTitle());
